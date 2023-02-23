f3230ff84e2e454f9fe8ed7b764163f41cf0a025
NSergeevich <niknikitaser@gmail.com>
2023-02-23 17:30:33 +0100

    max

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1;
+int b = 3;
+int c = 12;
+int d = 4;
+int e = 10;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write ("max = ");
+Console.Write (max);
