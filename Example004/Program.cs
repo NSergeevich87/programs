344c7481db0ffbe28421e677ddf7bfa319419aef
NSergeevich <niknikitaser@gmail.com>
2023-02-23 17:22:23 +0100

    vasya

---
+++ Example004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Put the name:");
+String name = Console.ReadLine ();
+if (name.ToLower () == "vasya")
+    {
+        Console.WriteLine ("Hi VasyaDurasya");
+        }
+else 
+{
+    Console.Write ("Hi,");
+    Console.Write (name);
+}
