d3d4cc36058736d6a86930a9ad9d67855c9a6d10
NSergeevich <niknikitaser@gmail.com>
2023-02-25 11:34:21 +0100

    009

---
+++ Example008_massifs/Program.cs
@@ -0,0 +1,19 @@
+﻿int max (int arg1, int arg2, int arg3)
+    {
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+    }
+//               0    1   2    3   4   5    6    7   8
+int[] array = {199, 232, 33, 464, 15, 63, 437, 866, 91};
+
+//array[0] = 987;
+
+int maximum = max (
+                   max(array[0], array[1], array[2]),
+                   max(array[3], array[4], array[5]),
+                   max(array[6], array[7], array[8])
+                   );
+
+Console.WriteLine(maximum);
---
+++ Example009_find_in_array/Program.cs
@@ -0,0 +1,17 @@
+﻿//programma nahogdeniya chisla v zadannom massive
+
+int[] array = {199,24,553,74,95,26,73,84};
+
+int n = array.Length;
+int find = 2;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
