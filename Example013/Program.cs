c4f074263c3818740939431decb4df9454e48a9d
NSergeevich <niknikitaser@gmail.com>
2023-03-05 20:12:26 +0100

    praktice

--- Example013/Program.cs
+++ Example013/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // Работа с текстом
 // Дан текст. В тексте нужно заменить все пробелы черточками,
 // маленькие буквы "к", заменить большими "К",
@@ -40,3 +40,30 @@ System.Console.WriteLine(newtext);
 newtext = replace(newtext, 'с', 'С');
 
 System.Console.WriteLine(newtext);
+*/
+
+string text = "Before he could answer, I scrambled up from the couch and lurched my way up"
+            + "the stairs. Someone had been in the house to leave a note for Charlie, a note"
+            + "that would lead him to find me.";
+
+string textedit(string text, char old, char mew)
+{
+    string result = string.Empty; 
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == old) result = result + $"{mew}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newtext = textedit(text, ' ', '!');
+
+System.Console.WriteLine(newtext);
+
+newtext = textedit(newtext, 'a', 'B');
+
+System.Console.WriteLine(newtext);
+
+
--- Example014/Program.cs
+++ Example014/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 // Сортировка элементов в массиве.
-
+/*
 int [] arr = {0, 3, 6, 5, 8, 4, 1, 5, 9, 2, 3, 8, 5, 3, 7, 6, 5};
 
 void PrintArray(int[] array)
@@ -36,5 +36,94 @@ PrintArray(arr);
 SelectionSort(arr);
 
 PrintArray(arr);
-
+*/
 // В рамках домашней работы сделать упорядочивание элементов от большого к малому.
+
+/*
+
+int [] arr = new int[15];
+
+void fillArray (int[] collection)
+{
+//int length = collection.Length;
+for (int i = 0; i < collection.Length; i++)
+{
+    collection[i] = new Random().Next(0, 10);
+}
+}
+
+void printArray (int[] collection)
+{
+   //int length = collection.Length;
+    
+    for (int i = 0; i < collection.Length; i++)
+    {
+        System.Console.Write($" {collection[i]}");
+    }
+}
+
+void sortArray (int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+    
+    int maxposition = i;
+
+    for (int j = i + 1; j < collection.Length; j++)
+    {
+        if (collection[j] > collection[maxposition])
+        {
+            maxposition = j;
+        }
+    }
+    
+    int timeposition = collection[i];
+    collection[i] = collection[maxposition];
+    collection[maxposition] = timeposition;
+    }
+}
+
+fillArray(arr);
+sortArray(arr);
+printArray(arr);
+
+*/
+
+int [] arr = new int[15];
+
+void fillArray (int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(0, 99);
+    }
+}
+
+void printArray (int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        System.Console.Write($" {col[i]}");
+    }
+}
+
+void placeArray (int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        int maxomin = i;
+
+        for (int j = i + 1; j < col.Length; j++)
+        {
+            if (col[j] < col[maxomin]) maxomin = j;
+        }
+
+        int timeplace = col[i];
+        col[i] = col[maxomin];
+        col[maxomin] = timeplace;
+    }
+}
+
+fillArray(arr);
+placeArray(arr);
+printArray(arr);
